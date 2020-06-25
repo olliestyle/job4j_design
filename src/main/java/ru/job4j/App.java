6bcf125db85f8d8a5f48b3df20f42ac2edee2a2a
olliestyle <olliestyle1989@gmail.com>
2020-06-25 22:22:03 +0300

    0. Инструменты[238814#303357].10

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -6,6 +6,7 @@ package ru.job4j;
  */
 public class App {
     public static void main(String[] args) {
+//        System.out.println("Hello World!");
         System.out.println("Hello World!");
     }
 }
