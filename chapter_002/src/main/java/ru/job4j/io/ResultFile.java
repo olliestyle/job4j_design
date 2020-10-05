a4d71b25766316373e8cce0c03a1f0bb33bca5d2
olliestyle <olliestyle1989@gmail.com>
2020-10-05 15:01:36 +0300

    CleanSome1

--- chapter_002/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -4,15 +4,8 @@ import java.io.*;
 
 public class ResultFile {
     public static void main(String[] args) throws IOException {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("1*2=2\n".getBytes());
-            out.write("1*3=3\n".getBytes());
-            out.write("1*4=4\n".getBytes());
-            out.write("1*5=5\n".getBytes());
-            out.write("1*6=6\n".getBytes());
-            out.write("1*7=7\n".getBytes());
-            out.write("1*8=8\n".getBytes());
-            out.write("1*9=9".getBytes());
+        try (PrintWriter pw = new PrintWriter(new BufferedOutputStream(new FileOutputStream("result.txt")))) {
+            pw.write("Hello, world");
         } catch (Exception e) {
             e.printStackTrace();
         }
