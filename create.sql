b14536134de9fb474d54c1085cc77d1628790bab
olliestyle <olliestyle1989@gmail.com>
2020-12-23 13:51:41 +0300

    1. Схема прав пользователи и роли.[1093#303331].1

--- create.sql
+++ create.sql
@@ -1,41 +1,45 @@
 create table Rules(
-    rule char(50) primary key
+    id serial primary key,
+    rule char(50) unique
 );
 create table Role(
-    role char(50) primary key
+    id serial primary key,
+    role char(50) unique
 );
 create table RoleRules(
-                          id serial primary key,
-                          char_role char(50) references role(role),
-                          char_rule char(50) references rules(rule)
+    id serial primary key,
+    id_role int references role(id),
+    id_rule int references rules(id)
 );
 create table Users(
-                      id serial primary key,
-                      name char(50),
-                      surname char(50),
-                      role_name char(50) references role(role)
+    id serial primary key,
+    name char(50),
+    surname char(50),
+    id_role int references role(id)
 );
 create table Category(
-    name char(50) primary key
+    id serial primary key,
+    name char(50) unique
 );
 create table State(
-    state char(50) primary key
+    id serial primary key,
+    state char(50) unique
 );
 create table Items(
-                      id serial primary key,
-                      description char(2000),
-                      user_id int references users(id),
-                      category_name char(50) references category(name),
-                      state_name char(50) references state(state)
+    id serial primary key,
+    description char(2000),
+    user_id int references users(id),
+    category_id int references category(id),
+    state_id int references state(id)
 );
 create table Comments(
-                         id serial primary key,
-                         comment char(2000),
-                         item_id int references items(id)
+    id serial primary key,
+    comment char(2000),
+    item_id int references items(id)
 );
 create table Attaches(
-                         id serial primary key,
-                         attach char(2000),
-                         item_id int references items(id)
+    id serial primary key,
+    attach char(2000),
+    item_id int references items(id)
 );
 
--- insert.sql
+++ insert.sql
@@ -2,47 +2,54 @@ insert into rules(rule) values ('create');
 insert into rules(rule) values ('read');
 insert into rules(rule) values ('update');
 insert into rules(rule) values ('delete');
-select * from rules;
+
 insert into role(role) values ('admin');
 insert into role(role) values ('root');
 insert into role(role) values ('client');
-select * from role;
-insert into rolerules(char_role, char_rule) values ('admin', 'create');
-insert into rolerules(char_role, char_rule) values ('admin', 'read');
-insert into rolerules(char_role, char_rule) values ('admin', 'update');
-insert into rolerules(char_role, char_rule) values ('client', 'read');
-insert into rolerules(char_role, char_rule) values ('root', 'create');
-insert into rolerules(char_role, char_rule) values ('root', 'read');
-insert into rolerules(char_role, char_rule) values ('root', 'update');
-insert into rolerules(char_role, char_rule) values ('root', 'delete');
-select * from rolerules;
-insert into users(name, surname, role_name) values ('Murat', 'Baibolatov', 'admin');
-insert into users(name, surname, role_name) values ('Egor', 'Egorov', 'client');
-insert into users(name, surname, role_name) values ('Pavel', 'Pavlov', 'client');
-insert into users(name, surname, role_name) values ('Ivan', 'Ivanov', 'client');
-insert into users(name, surname, role_name) values ('Denis', 'Denisov', 'root');
-select * from users;
+
+insert into rolerules(id_role, id_rule) values (1, 1);
+insert into rolerules(id_role, id_rule) values (1, 2);
+insert into rolerules(id_role, id_rule) values (1, 3);
+insert into rolerules(id_role, id_rule) values (3, 2);
+insert into rolerules(id_role, id_rule) values (2, 1);
+insert into rolerules(id_role, id_rule) values (2, 2);
+insert into rolerules(id_role, id_rule) values (2, 3);
+insert into rolerules(id_role, id_rule) values (2, 4);
+
+insert into users(name, surname, id_role) values ('Murat', 'Baibolatov', 1);
+insert into users(name, surname, id_role) values ('Egor', 'Egorov', 3);
+insert into users(name, surname, id_role) values ('Pavel', 'Pavlov', 3);
+insert into users(name, surname, id_role) values ('Ivan', 'Ivanov', 3);
+insert into users(name, surname, id_role) values ('Denis', 'Denisov', 2);
+
 insert into category(name) values ('Business');
 insert into category(name) values ('Sport');
 insert into category(name) values ('Food');
-select * from category;
+
 insert into state(state) values ('Active');
 insert into state(state) values ('Done');
 insert into state(state) values ('Queued');
-select * from state;
-insert into items(description, user_id, category_name, state_name)
-VALUES ('Have a breakfast', 1, 'Food', 'Queued');
-insert into items(description, user_id, category_name, state_name)
-VALUES ('Run 6 kilometers', 1, 'Sport', 'Done');
-insert into items(description, user_id, category_name, state_name)
-VALUES ('Call the boss', 1, 'Business', 'Active');
-select * from items;
-select * from items;
+
+insert into items(description, user_id, category_id, state_id)
+VALUES ('Have a breakfast', 1, 3, 3);
+insert into items(description, user_id, category_id, state_id)
+VALUES ('Run 6 kilometers', 1, 2, 2);
+insert into items(description, user_id, category_id, state_id)
+VALUES ('Call the boss', 1, 1, 1);
+
 insert into comments(comment, item_id) VALUES ('scrambled eggs with bacon', 1);
 insert into comments(comment, item_id) VALUES ('at the stadium, 5 o''clock', 2);
 insert into comments(comment, item_id) VALUES ('89111112122', 3);
-select * from comments;
+
 insert into attaches(attach, item_id) VALUES ('http:\\localhost\food\eda.png', 1);
 insert into attaches(attach, item_id) VALUES ('http:\\localhost\sport\krossovki.png', 2);
 insert into attaches(attach, item_id) VALUES ('http:\\localhost\business\boss.png', 3);
+select * from rules;
+select * from role;
+select * from rolerules;
+select * from users;
+select * from category;
+select * from state;
+select * from items;
+select * from comments;
 select * from attaches;
