2bfc9e1eb6d6f8feb3a89b624022cb45c50d7b78
olliestyle <olliestyle1989@gmail.com>
2021-04-03 03:16:16 +0300

    1. Что такое Сериализация? [#313163]

--- chapter_005/src/main/java/ru/job4j/solid/ocp/Thesaurus.java
+++ chapter_005/src/main/java/ru/job4j/solid/ocp/Thesaurus.java
@@ -1,10 +1,10 @@
-package ru.job4j.solid.ocp;
-
-public class Thesaurus {
-
-    // В данном случае принцип OCP нарушается в поле класса, которые конкретно относится только к одному языку
-    // Необходимо, чтобы поле было более абстрактным, например, просто Language language.
-
-    private EnglishLanguage englishLanguage;
-
-}
+//package ru.job4j.solid.ocp;
+//
+//public class Thesaurus {
+//
+//    // В данном случае принцип OCP нарушается в поле класса, которые конкретно относится только к одному языку
+//    // Необходимо, чтобы поле было более абстрактным, например, просто Language language.
+//
+//    private EnglishLanguage englishLanguage;
+//
+//}
