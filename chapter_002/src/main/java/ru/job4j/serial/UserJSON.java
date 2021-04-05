cefaf5142dcc0b86a0c4c4b02f9449c46e1c4bc6
olliestyle <olliestyle1989@gmail.com>
2021-04-05 23:34:21 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- chapter_002/src/main/java/ru/job4j/serial/UserJSON.java
+++ chapter_002/src/main/java/ru/job4j/serial/UserJSON.java
@@ -2,16 +2,21 @@ package ru.job4j.serial;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class UserJSON {
 
-    int age;
-    boolean isWork;
-    String name;
-    School school;
-    Relative[] relatives;
+    private int age;
+    private boolean isWork;
+    private String name;
+    private School school;
+    private Relative[] relatives;
 
     public UserJSON(int age, boolean isWork, String name, School school, Relative[] relatives) {
         this.age = age;
@@ -28,6 +33,18 @@ public class UserJSON {
         public Relative(String relative) {
             this.relative = relative;
         }
+
+        @Override
+        public String toString() {
+            return "Relative{" +
+                    "relative='" + relative + '\'' +
+                    '}';
+        }
+
+        public String toJsonString() {
+            return "{\"relative\":" + relative + "}";
+        }
+
     }
 
     public static class School {
@@ -37,6 +54,18 @@ public class UserJSON {
         public School(int number, boolean isGraduate) {
             this.number = number;
             this.isGraduate = isGraduate;
+        }
+
+        @Override
+        public String toString() {
+            return "School{" +
+                    "number=" + number +
+                    ", isGraduate=" + isGraduate +
+                    '}';
+        }
+
+        public String toJsonString() {
+            return "{\"number\":" + number + ",\"isGraduate\":" + isGraduate + "}";
         }
     }
 
@@ -52,23 +81,45 @@ public class UserJSON {
     }
 
     public static void main(String[] args) {
+
         final UserJSON userJSON = new UserJSON(25,
                 true,
                 "Ivan",
                 new School(111, true),
-                new Relative[]{new Relative("sister"), new Relative("brother")});
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(userJSON));
+                new Relative[]{new Relative("sister"), new Relative("brother"), new Relative("mother")});
 
-        final String myUserJson = "{\"age\":25," +
-                "\"isWork\":true," +
-                "\"name\":\"Jack\"," +
-                "\"school\":" +
-                "{\"number\":222,\"isGraduate\":false}," +
-                "\"relatives\":[{\"relative\":\"mother\"},{\"relative\":\"father\"}]" +
-                "}";
+        JSONObject userJSONObject = new JSONObject();
+        userJSONObject.put("age", userJSON.age);
+        userJSONObject.put("isWork", userJSON.isWork);
+        userJSONObject.put("name", userJSON.name);
+        JSONObject jsonSchool = new JSONObject(userJSON.school.toJsonString());
+        userJSONObject.put("school", jsonSchool);
 
-        final UserJSON myUserJSON = gson.fromJson(myUserJson, UserJSON.class);
-        System.out.println(myUserJson);
+        JSONArray relative = new JSONArray();
+        for (Relative r: userJSON.relatives) {
+            relative.put(new JSONObject(r.toJsonString()));
+        }
+        userJSONObject.put("relatives", relative);
+
+        System.out.println(userJSONObject.toString());
+
+//        final UserJSON userJSON = new UserJSON(25,
+//                true,
+//                "Ivan",
+//                new School(111, true),
+//                new Relative[]{new Relative("sister"), new Relative("brother")});
+//        final Gson gson = new GsonBuilder().create();
+//        System.out.println(gson.toJson(userJSON));
+//
+//        final String myUserJson = "{\"age\":25," +
+//                "\"isWork\":true," +
+//                "\"name\":\"Jack\"," +
+//                "\"school\":" +
+//                "{\"number\":222,\"isGraduate\":false}," +
+//                "\"relatives\":[{\"relative\":\"mother\"},{\"relative\":\"father\"}]" +
+//                "}";
+//
+//        final UserJSON myUserJSON = gson.fromJson(myUserJson, UserJSON.class);
+//        System.out.println(myUserJson);
     }
 }
